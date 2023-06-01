894dc858cf81fcdf3c3531a4e0ebdf9dd80c0c22
Zsr <1262426565@qq.com>
2023-06-01 22:01:46 +0800

    修改数据库文件,

--- study.sql
+++ study.sql
@@ -1,44 +1,123 @@
-/*
- Navicat Premium Data Transfer
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- 主机： 127.0.0.1
+-- 生成日期： 2023-06-01 15:56:16
+-- 服务器版本： 10.4.22-MariaDB
+-- PHP 版本： 8.0.15
 
- Source Server         : 本地数据库
- Source Server Type    : MySQL
- Source Server Version : 80033 (8.0.33)
- Source Host           : 192.168.1.88:3306
- Source Schema         : study
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
- Target Server Type    : MySQL
- Target Server Version : 80033 (8.0.33)
- File Encoding         : 65001
 
- Date: 29/04/2023 09:54:29
-*/
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+--
+-- 数据库： `soft`
+--
 
--- ----------------------------
--- Table structure for db_account
--- ----------------------------
-DROP TABLE IF EXISTS `db_account`;
-CREATE TABLE `db_account` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) DEFAULT NULL,
-  `username` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- --------------------------------------------------------
 
--- ----------------------------
--- Table structure for persistent_logins
--- ----------------------------
-DROP TABLE IF EXISTS `persistent_logins`;
+--
+-- 表的结构 `persistent_logins`
+--
+
 CREATE TABLE `persistent_logins` (
-  `username` varchar(64) NOT NULL,
-  `series` varchar(64) NOT NULL,
-  `token` varchar(64) NOT NULL,
-  `last_used` timestamp NOT NULL,
-  PRIMARY KEY (`series`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+                                     `username` varchar(64) NOT NULL,
+                                     `series` varchar(64) NOT NULL,
+                                     `token` varchar(64) NOT NULL,
+                                     `last_used` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-SET FOREIGN_KEY_CHECKS = 1;
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `soft_teacher`
+--
+
+CREATE TABLE `soft_teacher` (
+                                `id` int(11) NOT NULL,
+                                `name` text NOT NULL,
+                                `teacher_id` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- 转存表中的数据 `soft_teacher`
+--
+
+INSERT INTO `soft_teacher` (`id`, `name`, `teacher_id`) VALUES
+    (1, '杨飓风', 'ABCDEF');
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `soft_user`
+--
+
+CREATE TABLE `soft_user` (
+                             `id` int(11) NOT NULL,
+                             `email` varchar(20) NOT NULL,
+                             `username` varchar(30) NOT NULL,
+                             `password` varchar(100) NOT NULL,
+                             `is_stu` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- 转存表中的数据 `soft_user`
+--
+
+INSERT INTO `soft_user` (`id`, `email`, `username`, `password`, `is_stu`) VALUES
+                                                                              (1, '1262426565@qq.com', 'admin', '$2a$10$DCrVoLjBAoLacrqgpl4vaO4kX28hPMd9woQWVW0tkfTQYrnqeOjZS', 1),
+                                                                              (2, 'zhangshuui@gmail.com', '杨飓风', '$2a$10$Ks/E4dMUXST5w6QSW9MD/ekrw.0kDISYl3anq2owKVTAP4Gb8QuLK', 0);
+
+--
+-- 转储表的索引
+--
+
+--
+-- 表的索引 `persistent_logins`
+--
+ALTER TABLE `persistent_logins`
+    ADD PRIMARY KEY (`series`);
+
+--
+-- 表的索引 `soft_teacher`
+--
+ALTER TABLE `soft_teacher`
+    ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`) USING HASH,
+  ADD UNIQUE KEY `teacher_id` (`teacher_id`) USING HASH;
+
+--
+-- 表的索引 `soft_user`
+--
+ALTER TABLE `soft_user`
+    ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- 在导出的表使用AUTO_INCREMENT
+--
+
+--
+-- 使用表AUTO_INCREMENT `soft_teacher`
+--
+ALTER TABLE `soft_teacher`
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- 使用表AUTO_INCREMENT `soft_user`
+--
+ALTER TABLE `soft_user`
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
