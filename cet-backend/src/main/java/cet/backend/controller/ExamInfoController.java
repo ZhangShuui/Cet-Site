bb18bc6e9e2bd71adea538c022c85a7d602904f4
Zsr <1262426565@qq.com>
2023-07-01 21:40:19 +0800

    完成报考信息填写和考试支付

--- cet-backend/src/main/java/cet/backend/controller/ExamInfoController.java
+++ cet-backend/src/main/java/cet/backend/controller/ExamInfoController.java
@@ -1,8 +1,11 @@
 package cet.backend.controller;
 
+import cet.backend.entity.ExamRelated.ExamForQuit;
 import cet.backend.entity.ExamRelated.ExamInfo;
 import cet.backend.entity.ExamRelated.ExamInfoForGet;
 import cet.backend.entity.RestBean;
+import cet.backend.entity.apply.ApplyInfo;
+import cet.backend.mapper.ApplyInfoMapper;
 import cet.backend.service.impl.ExamInfoHandleServiceImpl;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.*;
@@ -26,9 +29,25 @@ public class ExamInfoController {
         return newList.subList(0, newList.size());
     }
 
+    List<ExamForQuit> convertQuit(List<ExamInfo> list, int user_id){
+        ArrayList<ExamForQuit> newList = new ArrayList<>();
+        Calendar calendar = Calendar.getInstance();
+        for (ExamInfo item : list) {
+            calendar.setTime(item.getStart_time());
+            calendar.add(Calendar.DATE, -7);
+            Timestamp ddl = new Timestamp(calendar.getTimeInMillis());
+            ApplyInfo curInfo = applyInfoMapper.findApplyInfoByMainKey(item.getExam_id(), user_id);
+            newList.add(new ExamForQuit(item.getExam_id(), item.getStart_time(), curInfo.getPayment_status(),ddl));
+        }
+        return newList.subList(0, newList.size());
+    }
+
 
     @Resource
     ExamInfoHandleServiceImpl service;
+
+    @Resource
+    ApplyInfoMapper applyInfoMapper;
 
     @PostMapping("/test/canTake")
     RestBean<List<ExamInfoForGet>> getExamInfoCanTake(@RequestParam("user_id") int user_id){
@@ -38,8 +57,8 @@ public class ExamInfoController {
 
 
     @PostMapping("/test/canQuit")
-    RestBean<List<ExamInfoForGet>> getExamInfoCanQuit(@RequestParam("user_id") int user_id){
+    RestBean<List<ExamForQuit>> getExamInfoCanQuit(@RequestParam("user_id") int user_id){
         return RestBean.success(
-                convert(service.getExamInfoCanQuit(user_id)));
+                convertQuit(service.getExamInfoCanQuit(user_id), user_id));
     }
 }
